2c8ca338fdd319368200555c64bdb462c6bcc4af
vldkvll <vldkvll.gmail.com>
2020-03-15 22:33:59 +0200

    Updates

---
+++ precache-manifest.893232270eef1af6add47b212755dab8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd73bcc2fc8c55276a411b1927bf7f5c",
+    "url": "/DemoSocialNet/index.html"
+  },
+  {
+    "revision": "cbb7db75adf2e41dd69a",
+    "url": "/DemoSocialNet/static/css/3.a833427c.chunk.css"
+  },
+  {
+    "revision": "12c8d7d85d17fe4cbd2d",
+    "url": "/DemoSocialNet/static/css/4.52694e22.chunk.css"
+  },
+  {
+    "revision": "49aad927a2d191ade8a6",
+    "url": "/DemoSocialNet/static/css/main.e8a88e6f.chunk.css"
+  },
+  {
+    "revision": "56cef2e73990cd78f231",
+    "url": "/DemoSocialNet/static/js/2.a329463e.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/DemoSocialNet/static/js/2.a329463e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb7db75adf2e41dd69a",
+    "url": "/DemoSocialNet/static/js/3.a2d545d5.chunk.js"
+  },
+  {
+    "revision": "12c8d7d85d17fe4cbd2d",
+    "url": "/DemoSocialNet/static/js/4.1c1efdd1.chunk.js"
+  },
+  {
+    "revision": "49aad927a2d191ade8a6",
+    "url": "/DemoSocialNet/static/js/main.b7010546.chunk.js"
+  },
+  {
+    "revision": "147169c2a1e69f6e6dc1",
+    "url": "/DemoSocialNet/static/js/runtime-main.4f8d1dc2.js"
+  },
+  {
+    "revision": "27a0f3e301780446c2923cadba348dc5",
+    "url": "/DemoSocialNet/static/media/DoubleRingPreloader_64px.27a0f3e3.svg"
+  },
+  {
+    "revision": "65fc0317ef6e364da52de1da0fa80a22",
+    "url": "/DemoSocialNet/static/media/avaIndianGirl.65fc0317.jpg"
+  },
+  {
+    "revision": "854af2e47aae3f575ab810dcce7b0215",
+    "url": "/DemoSocialNet/static/media/user-reducer-object-state-helper.854af2e4.bin"
+  }
+]);
