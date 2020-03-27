6486baf523a391e2ca08fab9f935594592ebf92b
vldkvll <vldkvll.gmail.com>
2020-03-27 21:30:58 +0200

    change HashRouter on BrowseRouter

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import store from "./MyRedux/redux-store";
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import App from "./App";
 import {Provider} from "react-redux";
 
 
 ReactDOM.render
 (
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>, document.getElementById('root')
+    </HashRouter>, document.getElementById('root')
 );
 
 
